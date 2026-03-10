7e287883e07b03d7eb4569cde3e595537a72ec51
Sahil <technology2903@gmail.com>
2026-03-10 11:20:36 +0530

    Connect with the datbase

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
+from database import db
+from models import User
+
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///manuai.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def home():
-    return "Welcome to ManuAI market"
+    return render_template('index.html')
 
 if(__name__) == "__main__":
     app.run(debug = True)
---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from database import db 
+
+class User(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    name = db.Column(db.String(200) , nullable = False)
+    email = db.Column(db.String(200) ,nullable = False ,unique = True)
+    password = db.Column(db.String(200) ,nullable = False)
+    role = db.Column(db.String(50),nullable = False)
