193c4638bf11487caa754d0c710c471a6b4b3000
Sahil <technology2903@gmail.com>
2026-03-10 16:41:59 +0530

    Make changes in the register form

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session,redirect
 from database import db
 from models import User
 
@@ -17,6 +17,31 @@ with app.app_context():
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/register" ,methods = ["GET" ,"POST"])
+def register():
+    if request.method == "POST":
+
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        role = request.form.get("role")
+
+        new_user = User(
+            name = name ,
+            email = email ,
+            password = password,
+            role = role
+        )
+
+        db.session.add(new_user)
+        db. session.commit()
+
+        return redirect("/")
+        
+    return render_template("register.html")
+
+
 
 if(__name__) == "__main__":
     app.run(debug = True)
